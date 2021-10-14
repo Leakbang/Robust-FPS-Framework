7098d8fa3d4d6994706cd47b6bd7de7f63a4960b
Leakbang <leakbang@protonmail.com>
2021-10-15 00:18:29 +0200

    Disable player controls when on Terminal

--- Project Files/player/interact/Terminal.gd
+++ Project Files/player/interact/Terminal.gd
@@ -1,18 +1,24 @@
 extends StaticBody
 
+var Player
+
 var BoxCount: int = 0
 
 func _ready():
+	Player = get_node("../Player")
 	$TerminalInterface.visible = false
 
 func _Interact():
 	$TerminalInterface.visible = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	Player.player_controls_enabled = false
+	
 	$TerminalInterface/ColorRect/Counter.text = String(BoxCount)
 
 func _on_Quit_pressed():
 	$TerminalInterface.visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Player.player_controls_enabled = true
 
 
 func _on_Area_body_entered(body):
