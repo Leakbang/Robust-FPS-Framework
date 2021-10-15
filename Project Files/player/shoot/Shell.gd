09f44b037b14a55e5f4602601145ddb9d723a99d
Leakbang <leakbang@protonmail.com>
2021-10-15 12:26:16 +0200

    Shells now bounce around and remain in the scene

--- Project Files/player/shoot/Shell.gd
+++ Project Files/player/shoot/Shell.gd
@@ -4,7 +4,12 @@ func _ready():
 	$ShellImpactSound.pitch_scale = rand_range(0.95, 1.05)
 
 func _on_LifetimeTimer_timeout():
-	queue_free()
+	self.set_sleeping(true)
+	print("now sleeping")
 
-func _on_AudioTimer_timeout():
+#func _on_AudioTimer_timeout():
+#	$ShellImpactSound.play()
+
+
+func _on_Shell_body_shape_entered(body_id, body, body_shape, local_shape):
 	$ShellImpactSound.play()
