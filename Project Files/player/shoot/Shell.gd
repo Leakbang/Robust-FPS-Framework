dba9ec7ef76ef6fae21735557cb3933821086223
Leakbang <leakbang@protonmail.com>
2021-10-15 12:46:38 +0200

    Minor adjustments
    Cleanup
    Move flashlight pos

--- Project Files/player/shoot/Shell.gd
+++ Project Files/player/shoot/Shell.gd
@@ -4,12 +4,7 @@ func _ready():
 	$ShellImpactSound.pitch_scale = rand_range(0.95, 1.05)
 
 func _on_LifetimeTimer_timeout():
-	self.set_sleeping(true)
-	print("now sleeping")
-
-#func _on_AudioTimer_timeout():
-#	$ShellImpactSound.play()
-
+	set_sleeping(true)
 
 func _on_Shell_body_shape_entered(body_id, body, body_shape, local_shape):
 	$ShellImpactSound.play()
