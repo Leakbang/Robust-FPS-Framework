17d9cd4e92d30b5b90d7491b008cc3945c3c76ec
Leakbang <leakbang@protonmail.com>
2021-10-20 00:36:33 +0200

    Play around with physics powered elevator

--- Project Files/scripts/Interactables/Elevator.gd
+++ Project Files/scripts/Interactables/Elevator.gd
@@ -11,9 +11,17 @@ func activate():
 
 func _process(delta):
 	if moving:
-		if body.get_global_transform().origin.y > 20:
+		var bounding_box: AABB = $Elevator/Floor.get_transformed_aabb()
+		var bounding_box_size: Vector3 = bounding_box.size
+		
+		var top = bounding_box.position.y + bounding_box_size.y / 2.0
+		var bottom = bounding_box.position.y - bounding_box_size.y / 2.0
+		
+		DrawDebug.draw_box(bounding_box.position, Vector3(bounding_box_size.x, bounding_box_size.y, bounding_box_size.z), Color(0, 1, 0))
+			
+		
+		if top > $Top.get_global_transform().origin.y:
+			body.sleeping = true;
 			going_up = false
 		if going_up:
 			body.add_central_force(Vector3(0, 300, 0))
-		else:
-			body.add_central_force(Vector3(0, 5, 0))
