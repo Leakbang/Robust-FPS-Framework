8e23ad9790ea980dcd43ec8826bf8b631fd11282
Leakbang <leakbang@protonmail.com>
2021-11-04 12:04:36 +0100

    Add basic crushable class

--- Project Files/scripts/interactables/breakable.gd
+++ Project Files/scripts/interactables/breakable.gd
@@ -1,21 +1,35 @@
 extends RigidBody
+class_name Breakable
 
-var threshold = 20.0
+export (float) var health = 100
+
+export (float) var threshold = 30.0
 
 func _integrate_forces(state):
 	var total_force = Vector3.ZERO
 	for i in range (0, state.get_contact_count()):
 		var object = state.get_contact_collider_object(i)
 		if object is RigidBody:
-			var normal = state.get_contact_local_normal(i)
-			var f_weight_normal = normal * normal.dot(Vector3(0, object.weight,0))
-			f_weight_normal = f_weight_normal.round()
-			#print(f_weight_normal)
-			# Force = mass * acceleration
 			var impulse = state.get_contact_impulse(i) * state.get_contact_local_normal(i)
+			impulse = impulse * 5
+			impulse = impulse.round()
 			total_force += impulse
 			
 			$Label.text = String(total_force)
 			
-			if total_force.x > threshold or total_force.y > threshold or total_force.z > threshold:
-				print("Broken")
+			if abs(total_force.x) > threshold:
+				print(total_force)
+				damage(abs(total_force.x)/2)
+			if abs(total_force.y) > threshold:
+				print(total_force)
+				damage(abs(total_force.y)/2)
+			if abs(total_force.z) > threshold:
+				print(total_force)
+				damage(abs(total_force.z)/2)
+	
+func damage(amount):
+	health -= amount
+	print("health:", health)
+	if health <= 0:
+		print("dead")
+		self.queue_free()
