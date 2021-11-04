75e8286d9e88913968edfaf401fe232ee4c0238d
Leakbang <leakbang@protonmail.com>
2021-11-04 12:04:48 +0100

    Update lab setup

--- Project Files/scripts/interactables/moving_door.gd
+++ Project Files/scripts/interactables/moving_door.gd
@@ -6,11 +6,14 @@ func activate():
 	move = true
 	print ("true")
 
-func _physics_process(delta):
+func _integrate_forces(state):
 	if move:
-		add_central_force(Vector3(100, 0, 0))
-#
-#func _integrate_forces(state):
-#	if move:
-#		print ("moving")
-#		add_central_force(Vector3(2100, 0, 0))
+		#TODO: Set the object to not sleeping
+		add_central_force(Vector3(200, 0, 0))
+		
+		for i in range (0, state.get_contact_count()):
+			var object = state.get_contact_collider_object(i)
+			if object is RigidBody or object == null:
+				pass
+			else:
+				move = false
