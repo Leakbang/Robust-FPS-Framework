cce1568772788ca494027c4f215aaed5aef502cf
Leakbang <leakbang@protonmail.com>
2021-11-06 11:11:24 +0100

    Proof of concept pickup system

--- Project Files/scripts/Player/Use/Use.gd
+++ Project Files/scripts/Player/Use/Use.gd
@@ -1,25 +1,37 @@
 extends RayCast
 
+onready var tween = $tween
 onready var UI = $"crosshair"
 onready var use_icon = $"hand_icon"
+onready var physics_joint = $use_pos/Joint
 
 var mass_limit = 50
 var throw_force = 20
 
-var last_used_item
+var can_use = true
 
 var object_grabbed = null
 var Target = null
-var can_use = true
-var text_visible = false
+var last_used_item = null
+
+
+var use_pos
+
+
+"""
+
+Replace prompt function with animations
+
+"""
 
 func _ready():
 	use_icon.set("visible", false)
 	UI.set("visible", false)
 
 func _physics_process(delta):
-	Target = get_collider();
-	if get_collider() and $UseTimer.is_stopped() and Target.is_in_group("Useable"):
+	Target = get_collider()
+	use_pos = get_collision_point()
+	if get_collider() and $use_timer.is_stopped() and Target.is_in_group("Useable"):
 		prompt()
 		UI.set("visible", false)
 	elif get_collider() and Target.owner.is_in_group("Terminal"):
@@ -28,7 +40,7 @@ func _physics_process(delta):
 		last_used_item = Target.owner
 		var v_c = get_collision_point ()
 		Target.owner.virutal_cursor = v_c
-	elif not object_grabbed and $UseTimer.is_stopped() and get_collider() is RigidBody and get_collider().mass <= mass_limit:
+	elif not object_grabbed and $use_timer.is_stopped() and get_collider() is RigidBody and get_collider().mass <= mass_limit:
 		prompt()
 		UI.set("visible", false)
 	else:
@@ -37,57 +49,42 @@ func _physics_process(delta):
 		UI.set("visible", true)
 		unprompt()
 		
-	# Drop object if it goes too far
-	if object_grabbed:
-		var vector = $UseRange.global_transform.origin - object_grabbed.global_transform.origin
-		object_grabbed.linear_velocity = vector * 10
-		object_grabbed.axis_lock_angular_x = true
-		object_grabbed.axis_lock_angular_y = true
-		object_grabbed.axis_lock_angular_z = true
+#Decrease mouse sensivity based on the weight of object
 
-		if vector.length() >= 5:
-			object_grabbed.set_mode(0)
-			release()
+	if object_grabbed:
+		object_grabbed.add_central_force(Vector3(0, 100,0))
 
 	# On key press
-	if Input.is_mouse_button_pressed(BUTTON_LEFT):
-		if can_use:
-			can_use = false
-			if get_collider() and $UseTimer.is_stopped() and Target.is_in_group("Useable"):
-				Target._Interact()
-				$UseTimer.start()
-			elif not object_grabbed:
-					if get_collider() is RigidBody and $UseTimer.is_stopped() and Target.mass <= mass_limit:
-						object_grabbed = Target
-						object_grabbed.rotation_degrees.x = 0
-						object_grabbed.rotation_degrees.z = 0
-			else:
-				release()
-	else:
-		can_use = true
-
-		if Input.is_mouse_button_pressed(BUTTON_RIGHT):
-			if object_grabbed:
-				object_grabbed.linear_velocity = global_transform.basis.z * -throw_force
-				release()
+	if Input.is_action_pressed("use"):
+		if get_collider() and Target is RigidBody:
+			object_grabbed = Target
+			var pth = object_grabbed.get_path()
+			physics_joint.set_node_b(pth)
+#			tween.interpolate_property(Target, "translation",Target.global_transform.origin, $use_pos.global_transform.origin, 0.1, 4, 2)
+#			tween.start()
+#			object_grabbed = Target
+#		if can_use:
+#			can_use = false
+		if get_collider() and $use_timer.is_stopped() and Target.is_in_group("Useable"):
+			Target._Interact()
+			$use_timer.start()
+			
+		
+	if Input.is_action_pressed("use_alt"):
+		# alternative use
+		release()
 		
 func release():
-	object_grabbed.axis_lock_angular_x = false
-	object_grabbed.axis_lock_angular_y = false
-	object_grabbed.axis_lock_angular_z = false
 	object_grabbed = null
-	$UseTimer.start()
-		
+	physics_joint.set_node_b("")
+
+"""
+Replace prompt function with animations
+"""
+
+
 func prompt():
-	if not text_visible:
-		text_visible = true
-		var animation_speed = 0.25
-		use_icon.set("visible", true)
-		$UseTween.start()
+	pass
 
 func unprompt():
-	if text_visible:
-		text_visible = false
-		var animation_speed = 0.25
-		use_icon.set("visible", false)
-		$UseTimer.start()
+	pass
