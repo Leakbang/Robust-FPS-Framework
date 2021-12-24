1191ab1b01863cc80489f783db9e37436fcaf13e
Leakbang <leakbang@protonmail.com>
2021-12-24 17:13:16 +0100

    Removed project files from Godot 3

--- Project Files/scripts/interactables/Door.gd
+++
@@ -1,8 +0,0 @@
-extends Spatial
-
-var roundedval = 0
-
-func _process(_delta):
-	roundedval = round($Door.rotation_degrees.y)
-	if roundedval == 0:
-		pass
--- Project Files/scripts/interactables/button.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody
-
-export (NodePath) var Target
-
-func _Interact():
-	var node = get_node(Target)
-	node.activate()
